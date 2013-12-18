6e69477409e03a7d42d4575f1041d2ee7b06df1c
Sumikawa Yasunobu <yasu3119nobu@yahoo.co.jp>
2013-12-18 19:47:21 +0900

    improve readability

--- eddpre/EQP.java
+++ eddpre/EQP.java
@@ -39,15 +39,12 @@ public class EQP implements LocalTransformer {
 	private SsaEnvironment env;
 	private SsaSymTab sstab;
 	private Function f;
-	BasicBlk[] bVecInOrderOfRPost;
+	public BasicBlk[] bVecInOrderOfRPost;
 	public GVN gvn;
-	EMemoryAliasAnalyze alias;
+	private EMemoryAliasAnalyze alias;
 	private DFST dfst;
 	public Dominators dom;
-	int idBound;
-	
-	Target target;
-	Print print;
+	public int idBound;
 	
 	/**
 	 * Constructor
@@ -82,8 +79,8 @@ public class EQP implements LocalTransformer {
 	HashSet[] isReal;
 	HashSet[] isSelf;
 	HashMap blkToNewNode;
-	public ArrayList newNodes;
-	public HashMap insertNodeToBlk;
+	public ArrayList<LirNode> newNodes;
+	public HashMap<LirNode, BasicBlk> insertNodeToBlk;
 	public int[] blkVal;
 	public boolean[] kill;
 	int mode;
@@ -100,7 +97,6 @@ public class EQP implements LocalTransformer {
 		return (true);
 	}
 	
-	
 	/**
 	 *  mode: 1 --> all redundant expressions are eliminated.
 	 *        2 --> MEM expressions are not eliminated.
@@ -108,13 +104,11 @@ public class EQP implements LocalTransformer {
 	 * @param mode
 	 */
 	public void invoke(int mode){
-		print = new Print(env,f);
 		set();
 		gvn(mode);
 		eliminate(mode);
 		alias.annul();
 	}
-	
 	
 	public void set(){
 		alias=new EMemoryAliasAnalyze(env,f);
@@ -129,11 +123,9 @@ public class EQP implements LocalTransformer {
 		collectInformation();
 	}
 	
-	
 	public void gvn(int mode){
 		gvn.gvn(mode);
 	}
-	
 	
 	private void collectInformation(){
 		for(int i=1;i<bVecInOrderOfRPost.length; i++) {
@@ -158,7 +150,6 @@ public class EQP implements LocalTransformer {
 		}
 	}
 	
-	
 	public void setVarsToGCM(BasicBlk blk, int val){
 		result = new HashSet[idBound];
 		avail = new int[idBound];
@@ -171,7 +162,6 @@ public class EQP implements LocalTransformer {
 		blkToNewNode = new HashMap();
 		blkVal = new int[idBound];
 	}
-	
 	
 	private void eliminate(int mode){
 		HashSet insertNodes = new HashSet();
@@ -199,7 +189,7 @@ public class EQP implements LocalTransformer {
 						valueMap.put(val, node.kid(j));
 					}
 				}
-				if(node.opCode!=Op.SET || insertNodes.contains(node))continue;
+				if(node.opCode!=Op.SET || insertNodes.contains(node)) continue;
 				
 				LirNode ve = gvn.makeVExp(node,blk);
 				int val = gvn.getValue(ve);
@@ -244,9 +234,8 @@ public class EQP implements LocalTransformer {
 				LirNode vnode = node.makeCopy(env.lir);
 				vnode.setKid(1, ve);
 				
-				if(propagate(val,vnode,copy,blk) && changeNameNewNodes(node,p)){
+				if(propagate(val,vnode,copy,blk) && changeNameNewNodes(node,p))
 					val = changeProg(node,ve,blk,val,p);
-				}
 				
 				gvn.setValue(val,node.kid(0),ve,blk);
 				valueMap.put(val, node.kid(0));
@@ -259,7 +248,6 @@ public class EQP implements LocalTransformer {
 			gvn.updateReachableValues(blk,blkValueMap);
 		}
 	}
-	
 	
 	public boolean propagate(int val, LirNode vnode, LirNode node, BasicBlk blk){
 		int True = 0;
@@ -316,9 +304,9 @@ public class EQP implements LocalTransformer {
 			}
 			
 			boolean localAnswer;
-			if(checkVisited(pred)){
-				if(checkResult(newVal,pred)) localAnswer = checkAvail(newVal,pred);
-				else localAnswer = visitedSameVal(newVal,pred);
+			if(visited.containsKey(pred)){
+				if(checkResult(newVal,pred)) localAnswer = (avail[pred.id]==newVal);
+				else localAnswer = (newVal==(Integer)visited.get(pred));
 			}else{
 				newNodes.put(pred, newNode);
 				blkVal[pred.id] = newVal;
@@ -334,7 +322,7 @@ public class EQP implements LocalTransformer {
 			}
 		}
 		recordResult(val,blk);
-		recordVisited(val,blk);
+		visited.put(blk, val);
 		boolean checkDSafe = mode==2 && Self!=blk.predList().length() || mode==3;
 		if(True>0){
 			if(blks.size()>0){
@@ -347,7 +335,7 @@ public class EQP implements LocalTransformer {
 				insertNewTempPhi(val,node,blk);
 				Real = blk.predList().length();
 			}
-			recordAvail(val,blk);
+			avail[blk.id] = val;
 			if(Real>0)recordIsReal(val,blk);
 			if(Self==blk.predList().length())recordIsSelf(val,blk);
 			return true;
@@ -355,12 +343,11 @@ public class EQP implements LocalTransformer {
 		return false;
 	}
 	
-	
 	private boolean local(int val, LirNode vnode, LirNode node, BasicBlk blk){
-		recordVisited(val,blk);
+		visited.put(blk, val);
 		boolean answer = false;
 		if(gvn.containValue(val,blk) && (node.kid(1).opCode!=Op.MEM || !kill[blk.id] || checkMemKill(val,node,blk))){
-			recordAvail(val,blk);
+			avail[blk.id] = val;
 			recordIsReal(val,blk);
 			if(blk==exprBlk)recordIsSelf(val,blk);
 			answer = true;
@@ -372,7 +359,6 @@ public class EQP implements LocalTransformer {
 		recordResult(val,blk);
 		return answer;
 	}
-	
 	
 	/**
 	 * Check local property of basic block which generate origin query.
@@ -388,69 +374,50 @@ public class EQP implements LocalTransformer {
 				if(n.kid(0).opCode==Op.MEM){
 					if(gvn.getValue(n.kid(1))==val) return true;
 					else break;
-				}else if(gvn.getValue(n.kid(0))==val)return true;
+				}else if(gvn.getValue(n.kid(0))==val) return true;
 			}
 			if(n.opCode==Op.CALL) break;
 		}
 		return false;
 	}
 	
-	
 	public void insertTempNewNode(LirNode node, int val, BasicBlk blk){
 		if(!insertNodeToBlk.containsValue(blk)){
 			node.setKid(0, createNewVar(node.kid(0),tmpSymName));
 			if(node.kid(1).opCode==Op.MEM) node.setKid(1, alias.makeNewMem(node.kid(1)));
 			node = node.makeCopy(env.lir);
-			addToNewNodes(node);
+			newNodes.add(node);
 			insertNodeToBlk.put(node, blk);
 			gvn.setValue(val, node.kid(0), blk);
 		}
 	}
 	
-	
 	public void insertTempNewNode(HashMap newNodes, ArrayList blks){
 		for(int i=0;i<blks.size();i++){
 			BasicBlk blk = (BasicBlk)blks.get(i);
 			int val = blkVal[blk.id];
-			if(val==-1)val = gvn.newValue();
+			if(val==-1) val = gvn.newValue();
 			LirNode node = (LirNode)newNodes.get(blk);
 			insertTempNewNode(node, val, blk);
 		}
 	}
 	
-	
 	public void insertNewTempPhi(int val, LirNode node, BasicBlk blk){
 		if(!existDomBlk(val,node.kid(1),blk)){
-			LirNode newPhi = null;
-			boolean remove = false;
-			if(newNodes.size()>0 && insertNodeToBlk.containsValue(blk)){
-				for(int i=0;i<newNodes.size();i++){
-					newPhi = (LirNode)newNodes.get(i);
-					if(newPhi.opCode!=Op.PHI || (((LirLabelRef) newPhi.kid(1).kid(2)).label).basicBlk()!=blk)continue;
-					newNodes.remove(i);
-					insertNodeToBlk.remove(newPhi);
-					gvn.removeValue(gvn.getValue(newPhi.kid(0)), newPhi.kid(0), blk);
-					remove = true;
-					break;
-				}
-			}
-			
-			if(!remove) newPhi = (newPhi(node,blk,tmpSymName)).makeCopy(env.lir);
-			
-			addToNewNodes(newPhi);
+			LirNode newPhi = (newPhi(node,blk,tmpSymName)).makeCopy(env.lir);
+			newNodes.add(newPhi);
 			newPhi = newPhi.makeCopy(env.lir);
 			insertNodeToBlk.put(newPhi, blk);
 			gvn.setValue(val, newPhi.kid(0), blk);
 		}
 	}
 	
-	
 	public boolean existDomBlk(int val, LirNode exp, BasicBlk blk){
 		for(BiLink p=blk.predList().first();!p.atEnd();p=p.next()){
 			BasicBlk pred = (BasicBlk)p.elem();
 			BasicBlk domBlk = pred;
 			while(domBlk!=null){
-				if(!checkVisited(domBlk) || val!=blkVal[domBlk.id]) return false;
+				if(!visited.containsKey(domBlk) || val!=blkVal[domBlk.id]) return false;
 				if(gvn.containValue(val, domBlk) && dom.dominates(domBlk, blk)) break;
 				if(insertNodeToBlk.containsValue(domBlk))return false;
 				domBlk = dom.immDominator(domBlk);
@@ -466,7 +433,6 @@ public class EQP implements LocalTransformer {
 		return list.contains(val);
 	}
 	
-	
 	public void recordResult(int val, BasicBlk blk){
 		if(result[blk.id]==null){
 			HashSet list = new HashSet();
@@ -476,22 +442,11 @@ public class EQP implements LocalTransformer {
 		list.add(val);
 	}
 	
-	
-	public boolean checkAvail(int val, BasicBlk blk){
-		return (avail[blk.id]==val);
-	}
-	
-	
-	public void recordAvail(int val, BasicBlk blk){
-		avail[blk.id] = val;
-	}
-		
 	public boolean checkIsReal(int val, BasicBlk blk){
 		if(isReal[blk.id]==null)return false;
 		HashSet list = isReal[blk.id];
 		return list.contains(val);
 	}
-	
 	
 	public void recordIsReal(int val, BasicBlk blk){
 		if(isReal[blk.id]==null){
@@ -502,13 +457,11 @@ public class EQP implements LocalTransformer {
 		list.add(val);
 	}
 	
-	
 	public boolean checkIsSelf(int val, BasicBlk blk){
 		if(isSelf[blk.id]==null)return false;
 		HashSet list = isSelf[blk.id];
 		return list.contains(val);
 	}
-	
 	
 	public void recordIsSelf(int val, BasicBlk blk){
 		if(isSelf[blk.id]==null){
@@ -519,40 +472,22 @@ public class EQP implements LocalTransformer {
 		list.add(val);
 	}
 	
-	
-	public boolean checkVisited(BasicBlk blk){
-		return visited.containsKey(blk);
-	}
-	
-	
-	public boolean visitedSameVal(int val, BasicBlk blk){
-		return (val==(Integer)visited.get(blk));
-	}
-	
-	
-	public void recordVisited(int val, BasicBlk blk){
-		visited.put(blk, val);
-	}
-	
-	
 	public LirNode createNewVar(LirNode typeNode, String tmpSymName){
 		Symbol dstSym = sstab.newSsaSymbol(tmpSymName, typeNode.type);
 		return env.lir.symRef(Op.REG, typeNode.type, dstSym, ImList.Empty);
 	}
-	
 	
 	public LirNode newPhi(LirNode expr, BasicBlk blk, String tmpSymName){
 		Symbol sym = sstab.newSsaSymbol(tmpSymName, expr.type);
 		return util.makePhiInst(sym, blk);
 	}
 	
-	
 	public boolean checkDSafe(int val, LirNode ve, LirNode expr, BasicBlk blk){
 		if(gvn.containValue(val,blk)) return true;
 		boolean[] checkBlk = new boolean[idBound];
 		for(BiLink p=blk.succList().first();!p.atEnd();p=p.next()){
 			BasicBlk succ = (BasicBlk)p.elem();
-			if(checkBlk[succ.id])continue;
+			if(checkBlk[succ.id]) continue;
 			LirNode newVExp = gvn.makeNewValueExp(ve,expr,succ);
 			int newVal = gvn.getValue(newVExp);
 			if(newVal==-1 || !checkDSafe(newVal,newVExp,expr,succ,checkBlk)) return false;
@@ -560,58 +495,30 @@ public class EQP implements LocalTransformer {
 		return true;
 	}
 	
-	
 	public boolean checkDSafe(int val, LirNode ve, LirNode expr, BasicBlk blk, boolean[] checkBlk){
 		checkBlk[blk.id] = true;
 		if(gvn.containValue(val,blk)) return true;
-		else if(blk==f.flowGraph().exitBlk()) return false;
-		else{
-			for(BiLink p=blk.succList().first();!p.atEnd();p=p.next()){
-				BasicBlk succ = (BasicBlk)p.elem();
-				if(checkBlk[succ.id])continue;
-				LirNode newVExp = gvn.makeNewValueExp(ve,expr,succ);
-				int newVal = gvn.getValue(newVExp);
-				if(newVal==-1 || !checkDSafe(newVal,newVExp,expr,succ,checkBlk)) return false;
-			}
-			return true;
+		if(blk==f.flowGraph().exitBlk()) return false;
+		for(BiLink p=blk.succList().first();!p.atEnd();p=p.next()){
+			BasicBlk succ = (BasicBlk)p.elem();
+			if(checkBlk[succ.id]) continue;
+			LirNode newVExp = gvn.makeNewValueExp(ve,expr,succ);
+			int newVal = gvn.getValue(newVExp);
+			if(newVal==-1 || !checkDSafe(newVal,newVExp,expr,succ,checkBlk)) return false;
 		}
+		return true;
 	}
-	
 	
 	boolean dependPhi(LirNode exp, BasicBlk blk){
 		for(int i=0;i<exp.nKids();i++){
 			if(exp.kid(i).opCode==Op.REG){
-				if(dependPhiSet.contains(exp.kid(i)))return true;
+				if(dependPhiSet.contains(exp.kid(i))) return true;
 			}else if(exp.kid(i).nKids()>0){
 				if(dependPhi(exp.kid(i),blk)) return true;
 			}
 		}
 		return false;
 	}
-	
-	public LirNode getVarLocal(int val, BiLink q){
-		LirNode var = null;
-		for(BiLink p=q;!p.atEnd();p=p.prev()){
-			LirNode node = (LirNode)p.elem();
-			if(node.opCode!=Op.SET && node.opCode!=Op.PHI)continue;
-			if(insertNodeToBlk.containsKey(node)){
-				if(node.kid(1).nKids()==0) var = node.kid(1);
-				else var = node.kid(0);
-				break;
-			}else if(node.kid(0).opCode==Op.REG){
-				if(gvn.getValue(node.kid(0))==val){
-					if(node.kid(1).nKids()==0) var = node.kid(1);
-					else var = node.kid(0);
-					break;
-				}
-			}else if(gvn.getValue(node.kid(1))==val){
-				var = node.kid(1);
-				break;
-			}
-		}
-		return var;
-	}
-	
 	
 	/**
 	 * return before node which has same value.
@@ -624,8 +531,26 @@ public class EQP implements LocalTransformer {
 		BasicBlk domBlk = blk;
 		if(q!=null){
 			// local
-			if(containNewNode(blk) || gvn.containValue(val,blk)){
-				LirNode var = getVarLocal(val,q);
+			if((blkToNewNode.containsKey(blk)) || gvn.containValue(val,blk)){
+				LirNode var = null;
+				for(BiLink p=q;!p.atEnd();p=p.prev()){
+					LirNode node = (LirNode)p.elem();
+					if(node.opCode!=Op.SET && node.opCode!=Op.PHI)continue;
+					if(insertNodeToBlk.containsKey(node)){
+						if(node.kid(1).nKids()==0) var = node.kid(1);
+						else var = node.kid(0);
+						break;
+					}else if(node.kid(0).opCode==Op.REG){
+						if(gvn.getValue(node.kid(0))==val){
+							if(node.kid(1).nKids()==0) var = node.kid(1);
+							else var = node.kid(0);
+							break;
+						}
+					}else if(gvn.getValue(node.kid(1))==val){
+						var = node.kid(1);
+						break;
+					}
+				}
 				if(var!=null) return var;
 			}
 			domBlk = dom.immDominator(blk);
@@ -634,86 +559,36 @@ public class EQP implements LocalTransformer {
 		while(domBlk!=null){
 			val = blkVal[domBlk.id];
 			if(gvn.containValue(val,domBlk)) return gvn.getVariable(val,domBlk);
-			else if(containNewNode(domBlk)) return getNewNode(domBlk).kid(0);
-			if(!checkAvail(val,domBlk)) break;
+			else if((blkToNewNode.containsKey(domBlk))) return ((LirNode)blkToNewNode.get(domBlk)).kid(0);
+			if(avail[domBlk.id]!=val) break;
 			domBlk = dom.immDominator(domBlk);
 		}
 		return null;
 	}
 	
-	
-	public boolean containNewNode(BasicBlk blk){
-		return (blkToNewNode.containsKey(blk));
-	}
-	
-	
-	public LirNode getNewNode(BasicBlk blk){
-		return (LirNode)blkToNewNode.get(blk);
-	}
-	
-	
-	public void removeFromNewNodes(LirNode node){
-		newNodes.remove(node);
-	}
-	
-	
-	public void addToNewNodes(LirNode node){
-		newNodes.add(node);
-	}
-	
-	
-	public BasicBlk getBlkFromInsertNodeToBlk(LirNode node){
-		if(!insertNodeToBlk.containsKey(node))return null;
-		else return (BasicBlk)insertNodeToBlk.get(node);
-	}
-	
-	
-	public void putEntryToInsertNodeToBlk(LirNode node, BasicBlk blk){
-		insertNodeToBlk.put(node, blk);
-	}
-	
-	
-	public void putEntryToBlkToNewNode(LirNode node, BasicBlk blk){
-		blkToNewNode.put(blk, node);
-	}
-	
-	
 	public int changeProg(LirNode node, LirNode ve, BasicBlk blk, int val, BiLink p){
 		LirNode predNode = getVar(val,blk,p.prev());
-		return changeProg(node,ve,predNode,blk,val,p);
-	}
-	
-	
-	public int changeProg(LirNode node, LirNode ve, LirNode predNode, BasicBlk blk, int val, BiLink p){
-		replace(node,predNode);
+		
+		// replace
+		LirNode dst = null;
+		if(predNode.opCode==Op.SET){
+			if(predNode.kid(0).opCode==Op.REG) dst = predNode.kid(0);
+			else dst = predNode.kid(1);
+		}
+		else if(predNode.opCode==Op.PHI){
+			dst = predNode.kid(0);
+		}
+		else{
+			dst = predNode;
+		}
+		node.setKid(1, dst.makeCopy(env.lir));
+		
+		//
 		int newVal = gvn.getValue(predNode);
 		if(newVal==-1)newVal = gvn.newValue();
 		if(val!=newVal)gvn.removeValue(node.kid(0),ve,val,blk);
 		return newVal;
 	}
-	
-	
-	public void replace(LirNode node, LirNode pred){
-		LirNode dst = null;
-		if(pred.opCode==Op.SET){
-			if(pred.kid(0).opCode==Op.REG) dst = pred.kid(0);
-			else dst = pred.kid(1);
-		}else if(pred.opCode==Op.PHI) dst = pred.kid(0);
-		else dst = pred;
-		node.setKid(1, dst.makeCopy(env.lir));
-	}
-	
-	
-	public void insert(LirNode node, BasicBlk blk){
-		for(BiLink p=blk.instrList().first();!p.atEnd();p=p.next()){
-			LirNode n = (LirNode)p.elem();
-			if(n.opCode!=Op.PHI){
-				p.addBefore(node);
-				break;
-			}
-		}
-	}
-	
 	
 	/**
 	 * change variables of new nodes.
@@ -722,12 +597,12 @@ public class EQP implements LocalTransformer {
 	 * @return
 	 */
 	public boolean changeNameNewNodes(LirNode expr, BiLink q){
-		HashSet newSetNode = new HashSet();
 		for(int i=0;i<newNodes.size();i++){
-			LirNode node = (LirNode)newNodes.get(i);
-			if(newSetNode.contains(node))continue;
-			BasicBlk blk = (BasicBlk)insertNodeToBlk.get(node);
-			if(node.opCode==Op.SET) insertNewNode(node.makeCopy(env.lir),blk,blk.instrList().last());
+			LirNode node = newNodes.get(i);
+			BasicBlk blk = insertNodeToBlk.get(node);
+			if(node.opCode==Op.SET){
+				insertNewNode(node.makeCopy(env.lir),blk,blk.instrList().last());
+			}
 			else{
 				node = insertNewPhi(node,blk);
 				if(node==null)return false;
@@ -739,12 +614,10 @@ public class EQP implements LocalTransformer {
 		return true;
 	}
 	
-	
 	public void insertNewNode(LirNode node, BasicBlk blk, BiLink p){
 		p.addBefore(node);
 		insertNodeToBlk.put(node, blk);
 	}
-	
 	
 	public LirNode insertNewPhi(LirNode phi, BasicBlk blk){
 		for(int j=1;j<phi.nKids();j++){
@@ -762,20 +635,19 @@ public class EQP implements LocalTransformer {
 		return copy;
 	}
 
-
 	public void newNodesNumbering(){
 		boolean change = true;
 		HashSet newValNode = new HashSet();
 		while(change){
 			change = false;
 			for(int i=0;i<newNodes.size();i++){
-				LirNode node = (LirNode)newNodes.get(i);
-				if(newValNode.contains(node))continue;
-				BasicBlk blk = (BasicBlk)insertNodeToBlk.get(node);
+				LirNode node = newNodes.get(i);
+				if(newValNode.contains(node)) continue;
+				BasicBlk blk = insertNodeToBlk.get(node);
 				int val = gvn.getValue(node.kid(0));
 				LirNode ve = gvn.makeVExp(node,blk);
 				int newVal = gvn.getValue(ve);
-				if(val!=-1 && val==newVal)continue;
+				if(val!=-1 && val==newVal) continue;
 				if(newVal==-1){
 					newVal = gvn.newValue();
 					newValNode.add(node);
@@ -787,25 +659,20 @@ public class EQP implements LocalTransformer {
 		}
 	}
 	
-	
-	public void cancel(LirNode var, BasicBlk blk){
-		for(BiLink p=blk.instrList().first();!p.atEnd();p=p.next()){
-			LirNode node = (LirNode)p.elem();
-			if(node.opCode!=Op.PHI && node.opCode!=Op.SET)continue;
-			if(node.kid(0).equals(var))p.unlink();
-		}
-	}
-	
-	
 	public void cancelInsertNode(){
 		for(int i=0;i<newNodes.size();i++){
-			LirNode node = (LirNode)newNodes.get(i);
+			LirNode node = newNodes.get(i);
 			if(!insertNodeToBlk.containsKey(node)) continue;
-			BasicBlk blk = (BasicBlk)insertNodeToBlk.get(node);
+			BasicBlk blk = insertNodeToBlk.get(node);
 			LirNode var = node.kid(0);
 			int val = gvn.getValue(var);
 			gvn.removeValue(val,node.kid(0),blk);
-			cancel(var,blk);
+			
+			for(BiLink p=blk.instrList().first();!p.atEnd();p=p.next()){
+				LirNode n = (LirNode)p.elem();
+				if(n.opCode!=Op.PHI && n.opCode!=Op.SET) continue;
+				if(n.kid(0).equals(var))p.unlink();
+			}
 		}
 	}
 }
